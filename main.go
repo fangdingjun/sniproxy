a55acea38feacf7069b6f333345ca11c7b020ef5
dingjun <fangdingjun@gmail.com>
2020-09-17 14:25:58 +0800

    add debug log and ignore ip sni

--- main.go
+++ main.go
@@ -157,11 +157,23 @@ func serve(ctx context.Context, c net.Conn) {
 		forward(ctx, c, buf[:n], getDefaultDST())
 		return
 	}
+
+	// sni is ip address, ignore it, prevent dead loop
+	ip := net.ParseIP(servername)
+	if ip != nil {
+		glog.Debugf("sni %s, send to default", ip.String())
+		forward(ctx, c, buf[:n], getDefaultDST())
+		return
+	}
+
 	dst := getDST(c, servername)
 	if dst == "" {
 		dst = getDefaultDST()
 		glog.Debugf("use default dst %s for sni %s", dst, servername)
 	}
+
+	glog.Debugf("from %s, sni %s, forward to %s", c.RemoteAddr(), servername, dst)
+
 	forward(ctx, c, buf[:n], dst)
 }
 
